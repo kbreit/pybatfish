99cf7c513c7effafba26921d632e22ff0d87c2ec
Spencer Fraint <spencer@intentionet.com>
2019-05-16 18:41:07 -0700

    add initial command running code

--- pybatfish/validation/commands.py
+++ pybatfish/validation/commands.py
@@ -13,15 +13,45 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 """Internal representation for validation commands."""
-from abc import ABCMeta
-from typing import List, Optional
+from abc import ABCMeta, abstractmethod
+from enum import Enum
+from typing import Any, Dict, List, Optional
 
 from six import add_metaclass
+
+from pybatfish.client.session import Session
+
+
+class CommandExecutionStatus(Enum):
+    """Enum for command execution status."""
+    SUCCESS = 1
+    FAILURE = 2
+    ERROR = 3
+
+
+class CommandResult(object):
+    """Result from execution of a command."""
+
+    def __init__(self, status, data):
+        # type: (CommandExecutionStatus, Dict[str, Any]) -> None
+        self.status = status
+        self.data = data
+
+    def to_dict(self):
+        # type: () -> Dict[str, Any]
+        return {
+            'status': self.status,
+            'data': self.data,
+        }
 
 
 @add_metaclass(ABCMeta)
 class Command(object):
     """Command abstract class."""
+
+    @abstractmethod
+    def run(self, session):
+        raise NotImplementedError()
 
 
 class InitSnapshot(Command):
@@ -33,6 +63,14 @@ class InitSnapshot(Command):
         self.upload = upload
         self.overwrite = overwrite
 
+    def run(self, session):
+        # type: (Session) -> CommandResult
+        result = session.init_snapshot(self.upload, self.name, self.overwrite)
+        return CommandResult(
+            CommandExecutionStatus.SUCCESS, {
+                'result': result
+            })
+
 
 class SetNetwork(Command):
     """Command to set current network."""
@@ -40,6 +78,14 @@ class SetNetwork(Command):
     def __init__(self, name):
         # type: (str) -> None
         self.name = name
+
+    def run(self, session):
+        # type: (Session) -> CommandResult
+        result = session.set_network(self.name)
+        return CommandResult(
+            CommandExecutionStatus.SUCCESS, {
+                'result': result
+            })
 
 
 class ShowFacts(Command):
@@ -49,6 +95,10 @@ class ShowFacts(Command):
         # type: (str) -> None
         self.nodes = nodes
 
+    def run(self, session):
+        # type: (Session) -> CommandResult
+        raise NotImplementedError()
+
 
 class CommandList(object):
     """Internal representation of a collection of commands."""
@@ -57,3 +107,19 @@ class CommandList(object):
         # type: (str, List[Command]) -> None
         self.name = name
         self.cmds = cmds
+
+    def run(self, session):
+        """Run the commands in this CommandList."""
+        return [self.run_command(cmd, session) for cmd in self.cmds]
+
+    def run_command(self, cmd, session):
+        """Run the specified command."""
+        try:
+            return cmd.run(session)
+        except Exception as e:
+            return CommandResult(
+                CommandExecutionStatus.ERROR,
+                {
+                    'result': '{}: {}'.format(e.__class__.__name__, e)
+                }
+            )
---
+++ pybatfish/validation/run.py
@@ -0,0 +1,37 @@
+# coding utf-8
+#   Copyright 2018 The Batfish Open Source Project
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+"""Handle user specified YAML 'policy' files."""
+
+import logging
+
+from pybatfish.client.session import Session
+from pybatfish.validation.commands import CommandList
+from pybatfish.validation.convert.yaml_c import convert_yaml
+
+# TODO remove this logging setup
+logging.getLogger('pybatfish.policy').setLevel(logging.INFO)
+logging.getLogger('pybatfish.policy').addHandler(logging.StreamHandler())
+
+
+def run_yaml(name, yaml_file, session=None):
+    logger = logging.getLogger(__name__)
+    logger.info('Creating command list: {}'.format(name))
+
+    if not session:
+        logger.info('No session supplied, creating one')
+        session = Session()
+
+    cmd_list = CommandList(name, convert_yaml(yaml_file))
+    return cmd_list.run(session)
